faf437ed9826379ed6101d9951cb362a116d8476
Teny23 <antoineboissinot@hotmail.com>
2021-03-05 17:42:15 -0500

    modif num7 et 8 en .py

--- TP2/numero_7.py
+++ TP2/numero_7.py
@@ -8,7 +8,8 @@ def p_nb_iter(R0,start,accuracy):
     #   start: Valeur de départ pour l'itérations allant de 0.1 à 1.4
     #   accuracy: précision de l'erreur de p
 
-    #--Retourne le nombre d'itération et la solution convergente à la précision préétablie
+    #--Retourne le nombre d'itération, la solution convergente à la précision préétablie selon la méthode de relaxation,
+    #--la valeur de R0 et la valeur de départ pour en faire des variables globales
     
     iteration = 1
     def f(p):
@@ -22,7 +23,7 @@ def p_nb_iter(R0,start,accuracy):
     while(abs(error(p1, p2)) > accuracy):
         p1, p2 = p2, f(p2)
         iteration += 1
-    print("Nombre d'itérations : ", iteration)
-    return p2
+    return iteration,p2,R0,start
 
-print(p_nb_iter(2,1,1e-6))
+
+print("Pour un nombre de reproduction de base (R_0) de {:} et une valeur de départ de {:}, on obtient un pourcentage \nde population (p) de {:.2f} %, en {:} itérations.".format(p_nb_iter(2,0.5,1e-6)[2],p_nb_iter(2,0.5,1e-6)[3],p_nb_iter(2,0.5,1e-6)[1]*100,p_nb_iter(2,0.5,1e-6)[0]))
--- TP2/numero_8.py
+++ TP2/numero_8.py
@@ -1,16 +1,17 @@
-def over_relax(R0, start, omega, accuracy):
+def over_relax(R0, omega,start, accuracy):
     #--Calcul du nombre d'itération du pourcentage de la population qui aura été infecté au terme de la pandémie
     #--Selon la méthode de relaxation accélérée
 
     #--Paramètres:
     #   R0: Nombre de reproduction de base 
-    #   start: Valeur de départ pour l'itérations allant de 0.1 à 1.4
     #   omega: paramètre de relaxation accélérée
+    #   start: valeur de départ pour l'itération allant de 0.1 à 1.4
     #   accuracy: précision de l'erreur de p
 
-    #--Retourne le nombre d'itération selon la méthode de relaxation accélérée
+    #--Retourne le nombre d'itération, la valeur finale de p selon la méthode de relaxation accélérée,
+    #--la valeur de R0 ainsi que la valeur de départ
     
-    iterations = 1
+    iteration = 1
 
     def f(p):
         return 1 - exp(- R0 * p)
@@ -25,8 +26,8 @@ def over_relax(R0, start, omega, accuracy):
     p2 = (1 + omega) * f(p1) - omega * p1
     while abs(error(p1, p2)) > accuracy:
         p1, p2 = p2, (1 + omega) * f(p2) - omega * p2
-        iterations += 1
-    return iterations
+        iteration += 1
+    return iteration,p2,R0,start
 
 def over_graph(R0,start,accuracy):
     #-- Calcul le graphique du nombre d'itération de la méthode de relaxation accélérée selon le paramètre omega partant 
@@ -34,7 +35,7 @@ def over_graph(R0,start,accuracy):
     
     #--Paramètres:
     #   R0: Nombre de reproduction de base 
-    #   start: Valeur de départ pour l'itérations allant de 0.1 à 1.4
+    #   start: valeur de départ pour l'itération allant de 0.1 à 1.4
     #   accuracy: précision de l'erreur de p
 
     #--Retourne un graphique du nombre d'itération selon le paramètre de relaxation accélérée
@@ -44,7 +45,7 @@ def over_graph(R0,start,accuracy):
     temp = linspace(0.5,2,16)
     
     for omega in temp:
-        iterations = over_relax(R0,start, omega, accuracy)
+        iterations = over_relax(R0,omega,start, accuracy)[0]
         
         y.append(iterations)
         
@@ -57,3 +58,4 @@ def over_graph(R0,start,accuracy):
     show()
     
 over_graph(2,1,1e-6)
+print("Selon la courbe ci-dessus, pour un nombre de reproduction de base (R_0) de {:} et une valeur de départ de {:}, la valeur \noptimale de omega est 0.5 ou 0.7, afin d'obtenir une valeur de p de {:.2f}% en seulement {:} itérations".format(over_relax(2, 0.5,1, 1e-6)[2],over_relax(2, 0.5,1, 1e-6)[3],over_relax(2, 0.5,1, 1e-6)[1]*100,over_relax(2, 0.5,1, 1e-6)[0]))
