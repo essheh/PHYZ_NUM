fffccad8806dc4c5a031629fd740e09712e0cc1f
essheh <francois.j.gaudreault@gmail.com>
2021-03-18 15:26:50 -0400

    Comète code terminé

--- TP3/Comet.py
+++ TP3/Comet.py
@@ -26,14 +26,14 @@ def Runge_Kutta(f, r, h):
     param 1 f: f is a vector of function
     param 2 r: r is the vector of the current coordinates
     param 3 h: h is the step between each coordinates 
-    return: r the updated coordinates vector 
+    return: dr the change of the coordinates vector 
     """
     k_1 = h*f(r)
     k_2 = h*f(r + 0.5*k_1)
     k_3 = h*f(r + 0.5*k_2)
     k_4 = h*f(r + k_3)
-    r += (k_1 + 2*k_2 + 2*k_3 + k_4)/6 
-    return r
+    dr = (k_1 + 2*k_2 + 2*k_3 + k_4)/6 
+    return dr
     
 
 def orbit_constant_step(x, y, v_x, v_y, t, h):
@@ -47,35 +47,35 @@ def orbit_constant_step(x, y, v_x, v_y, t, h):
     param 4 v_y: v_y is the speed of the comet in the y direction at time t = 0 [m/year] 
     param 5 t: t is the the time duration of the simulation [year]
     param 6 h: h is the time step between each time value 
-    return: x_points, y_points and time two array of the x and y position for each time t and one of the time duration with step h  
+    return: x_points, y_points and t_points, the x and y list of positions corresponding to the time of t_points   
     """ 
     t_points = np.arange(0, t, h) #The array of time values 
     x_points = [] #The list of x positions values
     y_points = [] #The list of y positions values
     r = np.array([x, y, v_x, v_y]) #The vector of all coordinates  
   
-    #The Runge-Kutta method starts from here 
+    #We use the Runge-Kutta method
     for t in t_points:
         x_points.append(r[0])
         y_points.append(r[1])   
-        r = Runge_Kutta(f, r, h) 
+        r += Runge_Kutta(f, r, h) 
     
-    return x_points, y_points
+    return x_points, y_points, t_points 
  
 x = 4*10**12 #[m]
 y = 0 
 v_x = 0
 v_y = 1000*(365.2422*24*60*60) # [m/year]
-t = 40 #[year]
+t = 70 #[year]
 h = 0.0008
 
-x_points, y_points = orbit_constant_step(x, y, v_x, v_y, t, h)
-plt.plot(x_points, y_points)
-plt.plot(0, 0, color = 'yellow', marker='o')
-plt.show()
+#x_points, y_points, t_points = orbit_constant_step(x, y, v_x, v_y, t, h)
+#plt.plot(x_points, y_points)
+#plt.plot(0, 0, color = 'yellow', marker='o')
+#plt.show()
 
 
-def time_step(x, y, v_x, v_y, t, h, delta):
+def orbit_variable_step(x, y, v_x, v_y, t, h, delta):
     """
     The function return the x and y position at time t of the orbit of a comet using the 4th order Runge-Kutta to solve Newton's law of universal
     gravitation with variable time step.    
@@ -90,28 +90,49 @@ def time_step(x, y, v_x, v_y, t, h, delta):
     return: x_points, y_points and time two array of the x and y position for each time t and one of the time duration with step h  
     """
     
-    t_points = np.arange(0, t, h) #The array of time values 
-    x_points = [] #The list of x positions values
-    y_points = [] #The list of y positions values
+    t_points = [0] #The list of time values 
+    x_points = [x] #The list of x positions values
+    y_points = [y] #The list of y positions values
     r = np.array([x, y, v_x, v_y]) #The vector of all coordinates 
+    current_t = 0 #The time at the step h 
     
-    step_1 = Runge_Kutta(f, r, h)
-    step_2 = Runge_Kutta(f, r + step_1, h)
-    total_step = step_1 + step_2
-    
-    double_step = Runge_Kutta(f, r, 2*h)
-    
-    x_error = total_step[0] - double_step[0]
-    y_error = total_step[1] - double_step[1]
-    error = np.sqrt(x_error**2 + y_error**2) 
-    rho = 30*h*delta/(error)
-    
-    if rho > 1: 
-            new_h = h*rho**(1/4)
-    else: 
+    while current_t < t:  
         
+        step_1 = Runge_Kutta(f, r, h) #First step of h
+        step_2 = Runge_Kutta(f, r + step_1, h) # Second step of h 
+        total_step = step_1 + step_2 # Total change of the two step of h 
+        
+        double_step = Runge_Kutta(f, r, 2*h) #Change of the step of 2h 
     
-delta = 1000 #[m]    
+        x_error = (total_step[0] - double_step[0]) / 30 #The error value of x
+        y_error = (total_step[1] - double_step[1]) / 30 #The error value of y 
+        
+        error = np.sqrt(x_error**2 + y_error**2) #The Euclidean error
+        
+        rho = 30*h*delta/(error) #Facteur permettant de déterminer la nouvelle valeur de h 
+        
+        h = h*rho**(1/4) #La nouvelle valeur de h 
+        
 
+        if rho >= 1: #Condition permettant de savoir si h est trop grand  
+            current_t = current_t + 2*h #On ajuste le temps 
+            
+            r += total_step + (total_step - double_step)/15 #Local extrapolation   
+            
+            x_points.append(r[0])
+            y_points.append(r[1])
+            t_points.append(current_t) 
+            
+        else:     
+            continue         
+        
+    return x_points, y_points, t_points 
+
+h = 0.0016        
+delta = 1000 #[m]    
+x_points, y_points, t_points = orbit_variable_step(x, y, v_x, v_y, t, h, delta)
+plt.plot(x_points, y_points)
+plt.plot(0, 0, color = 'yellow', marker='o')
+plt.show()
 
 
