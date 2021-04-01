48c6c50aab984ee3b4d1ddaf9d00c1be917f49a5
essheh <francois.j.gaudreault@gmail.com>
2021-04-01 15:45:24 -0400

    Numéro 1 et 2 fini

--- TP4/No1.py
+++ TP4/No1.py
@@ -7,19 +7,19 @@ hbar = constants.hbar #Planck's constant divided by 2*pi [m^2 kg / s]
 M = constants.electron_mass #Mass of the electron [kg]
 L = 1e-08 #Length of the box [m]
 x_0 = L/2 # [m]
-sigma = 1e-10 #[m^-1]
-kappa = 5e-10 #[m^-1]
+sigma = 1e-10 #[m]
+kappa = 5e10 #[m^-1]
 ######Number 1#######
 N = 1000 #Number of spatial slice 
 a = N/L  #Distance between each slice    
-h = 10**(-18)  #Step [s]
+h = 10**(-18)  #S       tep [s]
 a1 = 1 + 1j * h * hbar / (2 * M * a ** 2)
 a2 = - h * hbar * 1j / (4 * M * a ** 2)
 b1 = 1 - 1j * h * hbar / (2 * M * a ** 2)
 b2 = - a2
 
 def psi_0(x):
-    return np.exp(-(x-x_0)**2/2*sigma**2)*np.exp(1j*kappa*x) 
+    return np.exp(-(x-x_0)**2/2*sigma**2)*np.exp(1j*kappa*x)    
 
 
 def initial_condition(N, L, h): 
@@ -27,7 +27,7 @@ def initial_condition(N, L, h):
     x = np.linspace(0, L, N+1) 
     psi = np.array(psi_0(x)) #Initial condition  
     psi[0] = 0 
-    psi[-1] = 0
+    psi[-1] = 0 
     return psi 
     
     
@@ -40,12 +40,12 @@ def v(psi):
     
     return v
 
-psi = initial_condition(N, L, h)
+
 
 
 
 ######Number 2#######
-def A(N):
+def A_tri(N):
     A = np.zeros([3, N + 1], dtype = 'complex') #We write the tridiagonal matrix in the good form 
     A[0, 0] = 0
     A[0, 1:] = a2
@@ -54,29 +54,60 @@ def A(N):
     A[2, N] = 0
     return A 
     
-def Thomas(A, v, N): 
-    x = np.zeros(N+1, dtype = 'complex')
-    c_prime = np.zeros(N + 1, dtype = 'complex' )
-    d_prime = np.zeros(N + 1, dtype = 'complex' )
-    a_ = A[0, 0:] 
-    b_ = A[1, 0:]
-    c_ = A[2, 0:]
-    d_ = v[0:]
+def A(N):
+    A = np.zeros([N+1, N+1], dtype = 'complex')
+    for i in range(N+1):
+        A[i, i] = a1
+    for i in range(N):     
+        A[i, i+1] = a2 
+    for i in range(N):
+        A[i+1, i] = a2  
+    return A
     
-    c_prime[0] = c_[0]/b_[0]
-    c_prime[1: N] = c_[1: N]/(b_[1: N] - a_[1: N] * c_prime[0: N - 1])
-    d_prime[0] = d_[0]/b_[0] 
-    d_prime[1: N+1] = (d_[1: N+1] - a_[1: N+1] * d_prime[0: N])/(b_[1: N+1] - a_[1: N+1] * c_prime[0: N])
-    x[N] = d_prime[N]/b_[N]
-    x[N-1: :-1] = (d_prime[N-1: :-1] - c_prime[N-1: :-1]*x[N:0 :-1])/b_[N-1: :-1] 
+def Thomas(A_tri, v, N): 
+    c_prime = np.zeros(N, dtype = 'complex')
+    d_prime = np.zeros(N + 1, dtype = 'complex')
+    x = np.zeros(N + 1, dtype = 'complex')
     
-    return x, d_prime, d_
+    a_ = A_tri[0, :] 
+    b_ = A_tri[1, :]
+    c_ = A_tri[2, :]
+    d_ = v[:]
+    
+    c_prime[0] = c_[0]/b_[0] #First element of the new c' vector
+    d_prime[0] = d_[0]/b_[0] #First element of the new d' vector
+    
+    for i in range(1, N):   
+        c_prime[i] = c_[i]/(b_[i] - a_[i] * c_prime[i - 1])
+    
+    for i in range(1, N+1): 
+        d_prime[i] = (d_[i] - a_[i] * d_prime[i-1])/(b_[i] - a_[i] * c_prime[i-1])
+    
+    x[N] = d_prime[N]
+    
+    for i in range(N-1, -1, -1):
+        x[i] = d_prime[i] - c_prime[i]*x[i+1] 
+        
+    return x
 
+psi = initial_condition(N, L, h)
+v = v(psi)  
+A_tri = A_tri(N)    
+x = Thomas(A_tri, v, N)
 A = A(N)
-v = v(psi)
-x, d_prime, d_ = Thomas(A, v, N)
 Ax = np.matmul(A, x)
 print(x) 
-print(d_prime)
-print(d_)
+print(Ax)
+print(v)
 
+comparison = Ax == v
+equal_arrays = comparison.all()
+print(equal_arrays)
+
+
+#c_prime[0] = c_[0]/b_[0]
+#c_prime[1: N] = c_[1: N]/(b_[1: N] - a_[1: N] * c_prime[0: N - 1])
+#d_prime[0] = d_[0]/b_[0] 
+#d_prime[1: N+1] = (d_[1: N+1] - a_[1: N+1] * d_prime[0: N])/(b_[1: N+1] - a_[1: N+1] * c_prime[0: N])
+#x[N] = d_prime[N]
+#x[N-1: :-1] = (d_prime[N-1: :-1] - c_prime[N-1: :-1]*x[N:0 :-1])
